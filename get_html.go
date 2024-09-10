fc98c60470edc698b27a29f8882ffad05c4e5281
navivan123 <ivanfran49er@gmail.com>
2024-09-10 01:44:16 -0400

    Better error formatting

--- get_html.go
+++ get_html.go
@@ -19,10 +19,10 @@ func getHTML(rawURL string) (string, error) {
     defer res.Body.Close()
 
     if res.StatusCode >= http.StatusBadRequest {
-        return "", fmt.Errorf("%v | Status Code: %v", CodeHTTPError, res.StatusCode)
+        return "", fmt.Errorf(": %v | %v | Status Code: %v", rawURL, CodeHTTPError, res.StatusCode)
     }
     if res.Header.Get("Content-Type")[0:9] != "text/html" {
-        return "", fmt.Errorf("%v | Content-Type: %v", BadContentTypeHTTPError, res.Header.Get("Content-Type"))
+        return "", fmt.Errorf(": %v | %v | Content-Type: %v | Body: %v", rawURL, BadContentTypeHTTPError, res.Header.Get("Content-Type"), res.Body)
     }
 
     data, err := io.ReadAll(res.Body)
